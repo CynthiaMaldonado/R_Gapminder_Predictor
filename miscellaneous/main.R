522aad904b0470f8dcd5ae0f0000d33fe4eab36d
CynthiaMaldonado <cynthiamrdz@gmail.com>
2020-07-02 19:19:57 +0200

    Commit final 02072020

---
+++ miscellaneous/main.R
@@ -0,0 +1,5 @@
+lapply(c('XML', 'logging', 'reshape'), require, character.only = TRUE)
+
+path <- ('C:\Users\34671\Documents\GitHub\R_Gapminder_Predictor')
+
+R_Gapminder_Predictor <- paste0(path, "/R/start.R")
