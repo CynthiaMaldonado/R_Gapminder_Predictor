8569c46e6e1a5943f3ee60d7b0bd10a45a0aa135
gonzalo71 <gonzalofuste@gmail.com>
2020-07-09 12:58:48 +0200

    cambio forma de mergear los dataframe (merge all), y quito sep="\"

--- R/importardatos.R
+++ R/importardatos.R
@@ -18,15 +18,15 @@
   filenames <- c(config$data$predictors, config$data$target)
   # loop to open csv and append to list
   for (i in 1:length(filenames)){
-    url <- paste(path, filenames[i],sep = "/")
+    url <- paste(path, filenames[i])
     df <- data.table::fread(url,data.table = FALSE, header = TRUE)
    
     
     list_df[[i]] <- melt(df)
   }  
   # merging df
-  df_merge1 <- merge(list_df[[1]],list_df[[2]], by=c("country","variable"))
-  df_merge_final <- merge(df_merge1, list_df[[3]], by=c("country","variable")) 
-  colnames(df_merge_final)[5] <- "target"
+  
+  df_merge_final <- merge_all(list_df, by=c("country","variable"))
+  colnames(df_merge_final)[ncol(df_merge_final)] <- "target"
   return(df_merge_final)
 }
