24c4e37efd5e7ed3504ab398ddb408ea23c468ce
MigBor <mborreroperez@gmail.com>
2020-07-09 16:55:31 +0200

    Actualizo con logerrors

--- R/importardatos.R
+++ R/importardatos.R
@@ -11,11 +11,13 @@
 
 #' @import reshape
 #' @author gonzalo
-#' 
+
  importdata <- function(path,config){
   # creating an empty list and a list with all csv,s located in config
   list_df <- list()
   filenames <- c(config$data$predictors, config$data$target)
+  
+  tryCatch(expr = {
   # loop to open csv and append to list
   for (i in 1:length(filenames)){
     url <- paste(path, filenames[i])
@@ -24,6 +26,13 @@
     
     list_df[[i]] <- melt(df)
   }  
+  }, error = function(e){
+    
+    logerror("Data not found in that path. Check directory and config.",
+             logger = 'log')
+    stop()
+  })
+  
   # merging df
   
   df_merge_final <- merge_all(list_df, by=c("country","variable"))
