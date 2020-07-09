be8707e3436e2d1a8019952a3af20cde923a41f4
CynthiaMaldonado <cynthiamrdz@gmail.com>
2020-07-09 18:52:10 +0200

    Final commit 09/07/2020

---
+++ R/model.R
@@ -0,0 +1,58 @@
+
+#' @title regression model
+#' 
+#' @description Code to run the linear regression model.
+#' 
+#' @param df_merge_final, dataframe
+#' @param config
+#' 
+#' @export 
+#' @import MLmetrics
+#' 
+#' @return final_prediction
+
+#' @author Fran&Cyn
+
+regression_model <- function(df_merge_final, config){
+  
+  #Drop rows with NAs
+  
+  clean_df <- na.omit(df_merge_final)
+  
+  # Extract country and year to train the model
+  
+  clean_df <- clean_df[, 3:(ncol(clean_df))]
+  
+  #linear regression model. ATTENTION: y ~ x 
+  
+  object_model <- lm(target ~ . , data=clean_df)
+  
+  #predict WITH X
+  
+  output_value <- predict(object_model, df_merge_final[
+                (df_merge_final$country %in% country_user) & 
+                (df_merge_final$year %in% year_user), ]) 
+  
+  #make a comparison of predictions with test
+  
+  predictions_comparison <- RMSE(predictions, df_merge_final$target)
+  
+  #return (predictions_comparison)
+  return (output_value)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/prediction.R
@@ -0,0 +1,49 @@
+#' @title prediction
+#'
+#' @description search for the required output
+#'
+#' @param df_merge_final, dataframe
+#' @param config
+#'
+#' @export
+#' @import
+#'
+#' @return output_value
+#' @author Fran&Cyn
+#' 
+
+
+prediction <- function(df_merge_final, config){
+  # get year and country selected by the user.
+  
+  country_user <- config$data$country
+  year_user <- config$data$year
+  
+  target_value <- with(df_merge_final, target[country==country_user,
+                                              year==year_user])
+  
+  pred_file <- (df_merge_final$country %in% country_user) & 
+              (df_merge_final$year %in% year_user)
+  
+  check_nan <- df_merge_final[pred_file, 3:(ncol(df_merge_final)-1)]
+  
+  if(is.na(target_value) = FALSE){
+    
+    return(target_value)
+    }
+  
+  else{
+    
+    if(is.na(check_nan) = FALSE){
+      
+      output_pred <- regression_model(df_merge_final, config)
+      }
+    
+    else{
+      
+      output_impossible <- print('Sorry, we have not enough information to 
+                                 make a prediction.')
+      }
+    }
+}
+  
